f9d15f42096ae5188b5602db6c7eb19ee4f27f5e
vukhanh09 <vukhanh0920@gmail.com>
2021-12-20 20:38:27 +0700

    add

--- src/CrawlData/GetItemShopID.py
+++ src/CrawlData/GetItemShopID.py
@@ -2,8 +2,8 @@ import requests
 import json
 import pandas as pd
 from tqdm import tqdm
-def getItemShopID(match_id):
-    url_search_item = 'https://shopee.vn/api/v4/search/search_items?by=relevancy&limit=60&match_id={}&newest={0}'
+def getItemShopID(match_id,newest):
+    url_search_item = 'https://shopee.vn/api/v4/search/search_items?by=relevancy&limit=60&match_id={}&newest={}'
     
     def getItemShopId(data):
         itemId = data['item_basic']['itemid']
@@ -13,11 +13,12 @@ def getItemShopID(match_id):
             'shopid':shopId
         }
     
-    df = []
-    for newest in tqdm(range(0,8001,60)):
+    try:
         data = requests.get(url_search_item.format(match_id,newest)).content
         data = json.loads(data)['items']
         res = list(map(getItemShopId,data))
-        df += res
-    res = pd.DataFrame(df)
-    return res
+        return pd.DataFrame(res)
+    except:
+        print(f"An exception occurred with match_id {match_id} and newest {newest} ")
+    # res = pd.DataFrame(df)
+    # return res
